375bd1fa618ff4e5bd7006e5158626188c42540c
Ahmed Hossam <hossamiis.13@gmail.com>
2024-01-29 20:04:12 +0200

    change: added detailed pseudocode

--- firstSolution.js
+++ firstSolution.js
@@ -1,17 +1,24 @@
 const isPalindrome = (statment) => {
-  if (typeof statment !== "string") return false;
+  // firstly trim spaces and convert it to lowercase
   statment = statment.toLocaleLowerCase().trim();
 
-  // single char is also palindrome cause it satisfy reading from forward or backword
+  // single char is also palindrome cause it satisfy reading from forward or backword so return true (isPalindrome word)
   if (statment.length === 1) return true;
+
+  // doing regex to get rid of any thing is not a (word, number(digit) or even (_))
   let cleanedChars = statment.replace(/[^\w\d]|_/g, "");
-  //   const cleanedChars = cleanedPhrase.split("");
+
+  // creating two pointers from right and left
   let left = 0;
   let right = cleanedChars.length - 1;
+
+  // stop the while loop if they meets or cross
   while (left !== right) {
+    // check if characters at pointers do not match so return false (isn't palindrome word)
     if (cleanedChars[left] !== cleanedChars[right]) {
       return false;
     }
+    // in case of even length of word check if pointer next to each others that means it meets so return true (is palindrome word)
     if (right - left === 1) return true;
     left++;
     right--;
@@ -19,4 +26,5 @@ const isPalindrome = (statment) => {
   return true;
 };
 
-console.log(isPalindrome("Raadwdar"));
+// trigger isPalindrome function and log it's answer for testing
+console.log(isPalindrome("Raddar"));
--- secondSolution.js
+++ secondSolution.js
@@ -1,17 +1,29 @@
-const isPalindrome2 = (statment) => {
-  if (typeof statment !== "string") return false;
+const isPalindrome = (statment) => {
+  // firstly trim spaces and convert it to lowercase
   statment = statment.toLocaleLowerCase().trim();
+
   // single char is also palindrome cause it satisfy reading from forward or backword
   if (statment.length === 1) return true;
+
+  // doing regex to get rid of any thing is not a (word, number(digit) or even (_))
   let cleanedPhrase = statment.replace(/[^\w\d]|_/g, "");
+
+  // turning string into array
   const cleanedChars = cleanedPhrase.split("");
+
+  // creating another reversed array with another memory reference usin spread operator
   const reversedCleanedChars = [...cleanedChars].reverse();
+
+  // loop over two array and check element foreach one (original with reversed)
   for (let i = 0; i < cleanedChars.length; i++) {
+    // if there was dofference between char and the reversed one means it cann't be palindrome so return false
     if (cleanedChars[i] !== reversedCleanedChars[i]) {
       return false;
     }
   }
+  // if it passed without any proble it will be palidrome and return true
   return true;
 };
 
-console.log(isPalindrome2("Radssdar"));
+// trigger isPalindrome function and log it's answer for testing
+console.log(isPalindrome("Radssdar"));
